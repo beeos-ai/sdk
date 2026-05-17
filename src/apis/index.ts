644aed5f949a17bca96cd908205e9ace87a60935
BeeOS CI <ci@beeos.ai>
2026-05-17 10:06:54 +0000

    chore(sync): regenerate @beeos-ai/sdk v0.1.0 from openapi-gateway contract
    Source: beeos-ai/openagent@f3a160dc9781bc5cd74edd12dfd57fd114eb07c1
    
    Backend: 46b1a5612fcfe3c0cacbf18c8a5547ab2cb290ab
    
    Spec: sdks/openapi-sdk/spec/beeos-platform-v1.yaml

---
+++ src/apis/index.ts
@@ -0,0 +1,5 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AgentsApi';
+export * from './DeployApi';
+export * from './InstancesApi';
---
+++ src/models/index.ts
@@ -0,0 +1,22 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AgentDTO';
+export * from './AgentListResponse';
+export * from './AgentResponse';
+export * from './DeployInstanceRequest';
+export * from './DeployModelItem';
+export * from './DeployModelsListResponse';
+export * from './DeployRegionItem';
+export * from './DeployRegionsListResponse';
+export * from './InstanceDataDTO';
+export * from './InstancePageResponse';
+export * from './InstanceResponse';
+export * from './InvokeAgentRequest';
+export * from './InvokeAgentResponse';
+export * from './InvokeAgentResponseAllOfData';
+export * from './ProviderCapabilities';
+export * from './ProviderCatalogItem';
+export * from './ProviderMeta';
+export * from './ProvidersListResponse';
+export * from './StringResponse';
+export * from './SuccessEnvelope';
