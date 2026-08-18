d1ea112d7018dcaa9cac4e67138feac322328d2f
BeeOS CI <ci@beeos.ai>
2026-08-18 11:00:42 +0000

    chore(sync): regenerate @beeos-ai/sdk v0.5.0 from openapi-gateway contract
    Source: beeos-ai/openagent@1fc0e3cccd69b3ed98b40e45c3cbdc54d0f61e38
    
    Backend: 81b7064f56feb591f654a6d7766af9c74bd31a91
    
    Spec: sdks/openapi-sdk/spec/beeos-platform-v1.yaml

---
+++ src/apis/index.ts
@@ -0,0 +1,11 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AgentsApi';
+export * from './ComputerApi';
+export * from './ConversationsApi';
+export * from './DefaultApi';
+export * from './DeployApi';
+export * from './FilesApi';
+export * from './InstancesApi';
+export * from './MobileApi';
+export * from './TasksApi';
